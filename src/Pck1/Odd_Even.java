bfc50e90c3418e6aab381df18fc0bc7e6be8a29b
91982 <91982@LAPTOP-N6R24P62>
2021-12-19 21:57:12 +0530

    HI.....iam gitu

---
+++ src/Pck1/Odd_Even.java
@@ -0,0 +1,21 @@
+package Pck1;
+
+public class Odd_Even {
+
+	public static void main(String[] args) {
+		int a=21;
+		System.out.println(a/2);// dividetion 
+		System.out.println(a%2);//Quosiont
+		
+		
+		if(a%2==0){
+			System.out.println("value is even");
+			
+		}
+		else {
+			System.out.println("Value is odd");
+		}
+
+	}
+
+}
